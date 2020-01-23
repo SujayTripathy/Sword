27e6affcb82f91a34d99d38c79b8d6ff1b11e74c
SujayTripathy <36439006+champfinal@users.noreply.github.com>
2020-01-22 22:14:25 -0700

    sword rotation, projectiles

---
+++ SwordCombat/Assets/Projectile.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Rigidbody rb;
+    void Start()
+    {
+        rb=GetComponent<Rigidbody>();
+        Destroy(gameObject,5);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision collision){
+        rb.useGravity=true;
+    }
+}
---
+++ SwordCombat/Assets/Shooter.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooter : MonoBehaviour
+{
+    public GameObject cube;
+    public float speed=70;
+    float time=2f;
+    float initialTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        initialTime=Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   
+
+        if(Time.time-initialTime>=time){
+            GameObject now=Instantiate(cube,transform.position+new Vector3(Random.Range(-10,10),0,0),transform.rotation);
+            now.GetComponent<Rigidbody>().AddForce(now.transform.forward*speed,ForceMode.VelocityChange);
+            now.GetComponent<Rigidbody>().AddTorque(new Vector3(Random.Range(-1,1),Random.Range(-1,1),Random.Range(-1,1)),ForceMode.VelocityChange);
+            speed+=1f;
+            initialTime=Time.time;
+        }
+    }
+}

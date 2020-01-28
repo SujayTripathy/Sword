773a2ae6d069a91807eb7bb4b73f9d567feee130
SujayTripathy <36439006+champfinal@users.noreply.github.com>
2020-01-28 06:37:15 -0700

    trail, touch simulation, menu
    Added a trail renderer to the sword, a basic menu screen and play button which starts the game when clicked, and changed the sword to only rotate when the mouse is held down and moved

--- SwordCombat/Assets/Sword.cs
+++ SwordCombat/Assets/Sword.cs
@@ -4,18 +4,20 @@ using UnityEngine;
 
 public class Sword : MonoBehaviour
 {
+    AudioSource slash;
     public float speed=10;
     // Start is called before the first frame update
     void Start()
     {
-
+        slash=GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(new Vector3(0,0,Input.GetAxis("Mouse X"))*speed*Time.deltaTime);
-        transform.Rotate(new Vector3(Input.GetAxis("Mouse Y"),0,0*speed*Time.deltaTime),Space.World);
+        if(Input.GetMouseButton(0))
+            transform.Rotate(new Vector3(0,0,Input.GetAxis("Mouse X"))*speed*Time.deltaTime);
+        //transform.Rotate(new Vector3(Input.GetAxis("Mouse Y"),0,0*speed*Time.deltaTime),Space.World);
         
     }
 }

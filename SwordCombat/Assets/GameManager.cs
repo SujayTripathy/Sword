495048695326bcf4807934144961b33954edd96f
SujayTripathy <36439006+champfinal@users.noreply.github.com>
2020-01-31 06:40:44 -0700

    added sword trail and scoring

---
+++ SwordCombat/Assets/GameManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float score;
+    public GameObject scoreText;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        scoreText.GetComponent<Text>().text=score.ToString();
+    }
+}
--- SwordCombat/Assets/Projectile.cs
+++ SwordCombat/Assets/Projectile.cs
@@ -6,19 +6,25 @@ public class Projectile : MonoBehaviour
 {
     // Start is called before the first frame update
     Rigidbody rb;
+    public float scoreValue=100;
+    public GameManager scoreTracker;
     void Start()
     {
         rb=GetComponent<Rigidbody>();
         Destroy(gameObject,5);
+        scoreTracker=FindObjectOfType<GameManager>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     void OnCollisionEnter(Collision collision){
-        rb.useGravity=true;
+        if(collision.transform.tag=="Sword"){
+            rb.useGravity=true;
+            scoreTracker.score+=scoreValue;
+        }
     }
 }
